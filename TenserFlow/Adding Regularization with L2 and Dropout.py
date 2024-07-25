fd0a0f87919c6e301dbf6e6b670378a1de498efc
Tushar k <tushar.kale@mitcorer.edu.in>
2024-07-25 20:53:20 +0530

    Adding Regularization with L2 and Dropout

---
+++ TenserFlow/Adding Regularization with L2 and Dropout.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, regularizers
+from tensorflow.keras.datasets import cifar10
+
+# Load the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Normalize the pixel values
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
+
+# Define the first model
+model1 = keras.Sequential(
+    [
+        keras.Input(shape=(32, 32, 3)),
+        layers.Conv2D(32, 3, padding='valid', activation='relu'),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, 3, activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(128, 3, activation='relu'),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(10),
+    ]
+)
+
+# Print the model summary
+model1.summary()
+
+# Define the second model with batch normalization and dropout
+def my_model():
+    inputs = keras.Input(shape=(32, 32, 3))
+    x = layers.Conv2D(32, 3)(inputs)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.MaxPooling2D()(x)
+    x = layers.Conv2D(64, 5, padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Conv2D(
+        128, 3, padding='same', kernel_regularizer=regularizers.l2(0.01))(x)
+    x = layers.BatchNormalization()(x)
+    x = keras.activations.relu(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.01))(x)
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(10)(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+# Create the model
+model2 = my_model()
+
+# Compile the model
+model2.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(learning_rate=3e-4),
+    metrics=["accuracy"],
+)
+
+# Train the model
+model2.fit(x_train, y_train, batch_size=64, epochs=10, verbose=2)
+
+# Evaluate the model
+model2.evaluate(x_test, y_test, batch_size=64, verbose=2)
